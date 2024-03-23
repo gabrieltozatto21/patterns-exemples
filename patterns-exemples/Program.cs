363639b8651bf1aa65e27d1abe184e9791e58846
gabrieltozatto21 <gabriel.tozatto@hotmail.com>
2024-03-23 17:26:22 -0300

    first commit

---
+++ patterns-exemples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace patterns_exemples
+{
+    class Program
+    {
+        public class ChaveValor
+        {
+            [JsonPropertyName("chave")]
+            public string Chave { get; set; }
+            [JsonPropertyName("valor")]
+            public string Valor { get; set; }
+
+        }
+        public class EntidadeTeste
+        {
+            [JsonPropertyName("chave")]
+            public string Nome { get; set; }
+
+        }
+        static void Main(string[] args)
+        {
+            string json = "{\"chave\": \"Nome\",\"valor\": \"Gabriel\"}";
+
+            ChaveValor dicionario = JsonSerializer.Deserialize<ChaveValor>(json);
+
+            //EntidadeTeste entidade = new EntidadeTeste();
+
+            Type tipo = Type.GetType("patterns_exemples.Program+EntidadeTeste");
+
+            object instanciaCliente = Activator.CreateInstance(tipo);
+
+            var propriedade = tipo.GetProperty(dicionario.Chave);
+
+            propriedade.SetValue(instanciaCliente, dicionario.Valor);
+
+        }
+    }
+}
