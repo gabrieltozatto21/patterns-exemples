4efc9c318f53fdc28c694ee2e0061277736383ee
Gabriel Tozatto <gabriel.tozatto@hotmail.com>
2026-03-17 06:50:23 -0300

    commit

---
+++ LongestSubstringNoRepeatChars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSubstringNoRepeatChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LevelOrder(new Node(1, new List<Node> { new Node(2, new List<Node>()), new Node(3, new List<Node> { new Node(6), new Node(7, new List<Node> { new Node(11, new List<Node> { new Node(14) }) }) }), new Node(4, new List<Node> { new Node(8, new List<Node> { new Node(12) })}), new Node(5, new List<Node> { new Node(9, new List<Node> { new Node(13) }), new Node(10) }) })));
+            // Console.WriteLine(MissingInteger(new int[] {3,4,5,1,12,14,13,15,16,17,18,19,20,21}));
+            // Console.WriteLine(MissingInteger(new int[] {4,5,6,7,8,8,9,4,3,2,7}));
+            // Console.WriteLine(MissingInteger(new int[] {38}));
+            // Console.WriteLine(MissingInteger(new int[] {46,8,2,4,1,4,10,2,4,10,2,5,7,3,1}));
+            // Console.WriteLine(MissingInteger(new int[] {3,4,5,1,12,14,13}));
+            // Console.WriteLine(MissingInteger(new int[] {1,2,3,9,2,10,8,3,10,2}));
+            // Console.WriteLine(MissingInteger(new int[] {1,2,3,9,8,7,6,5,4,3,10}));
+        }
+
+        public int LengthOfLongestSubstring(string s) {
+            int maxLength = 0;
+            int left = 0;
+            
+        }
+
+    }
+}
+
--- patterns-exemples/Program.cs
+++ patterns-exemples/Program.cs
@@ -1,41 +1,71 @@
 ﻿using System;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using System.Collections.Generic;
 
 namespace patterns_exemples
 {
     class Program
     {
-        public class ChaveValor
-        {
-            [JsonPropertyName("chave")]
-            public string Chave { get; set; }
-            [JsonPropertyName("valor")]
-            public string Valor { get; set; }
-
-        }
-        public class EntidadeTeste
-        {
-            [JsonPropertyName("chave")]
-            public string Nome { get; set; }
-
-        }
         static void Main(string[] args)
         {
-            string json = "{\"chave\": \"Nome\",\"valor\": \"Gabriel\"}";
+            Console.WriteLine(LevelOrder(new Node(1, new List<Node> { new Node(2, new List<Node>()), new Node(3, new List<Node> { new Node(6), new Node(7, new List<Node> { new Node(11, new List<Node> { new Node(14) }) }) }), new Node(4, new List<Node> { new Node(8, new List<Node> { new Node(12) })}), new Node(5, new List<Node> { new Node(9, new List<Node> { new Node(13) }), new Node(10) }) })));
+            // Console.WriteLine(MissingInteger(new int[] {3,4,5,1,12,14,13,15,16,17,18,19,20,21}));
+            // Console.WriteLine(MissingInteger(new int[] {4,5,6,7,8,8,9,4,3,2,7}));
+            // Console.WriteLine(MissingInteger(new int[] {38}));
+            // Console.WriteLine(MissingInteger(new int[] {46,8,2,4,1,4,10,2,4,10,2,5,7,3,1}));
+            // Console.WriteLine(MissingInteger(new int[] {3,4,5,1,12,14,13}));
+            // Console.WriteLine(MissingInteger(new int[] {1,2,3,9,2,10,8,3,10,2}));
+            // Console.WriteLine(MissingInteger(new int[] {1,2,3,9,8,7,6,5,4,3,10}));
+        }
 
-            ChaveValor dicionario = JsonSerializer.Deserialize<ChaveValor>(json);
+        public static IList<IList<int>> LevelOrder(Node root) {
+            IList<IList<int>> result = new List<IList<int>>();
 
-            //EntidadeTeste entidade = new EntidadeTeste();
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
 
-            Type tipo = Type.GetType("patterns_exemples.Program+EntidadeTeste");
+            while (queue.Count > 0)
+            {
+                IList<int> childResult = new List<int>();
 
-            object instanciaCliente = Activator.CreateInstance(tipo);
+                int length = queue.Count;
+                
+                for (int i = 0; i < length; i++)
+                {
+                    Node node = queue.Dequeue();
+                    childResult.Add(node.val);
 
-            var propriedade = tipo.GetProperty(dicionario.Chave);
+                    if (node == null || node.children == null)
+                        continue;
 
-            propriedade.SetValue(instanciaCliente, dicionario.Valor);
+                    foreach(Node child in node.children)
+                    {
+                        queue.Enqueue(child);
 
+                    }   
+                }
+
+                result.Add(childResult);
+
+            } 
+            return result;
+        }
+
+    }
+
+    public class Node {
+        public int val;
+        public IList<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children) {
+            val = _val;
+            children = _children;
         }
     }
 }
+
